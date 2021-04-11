1d69605129137a71d29aea7e775fe87695f05bc3
Hervé Eruam <herve.eruam@etu.univ-lyon1.fr>
2021-04-11 13:54:50 +0200

    Module HitBox ajouté Stock les coordonnées d'une rectangle (x1 x2 y1 y2) est possede trois fonction membres pour savoir si les parametre x,y sont dans le rectangle, getHauteur et get Largeur Utile pour passer les coordonné entre les fonction d'affichage et les fonctions qui gere les evenements(pour les clics souris)

---
+++ src/sdl/HitBox.cpp
@@ -0,0 +1,37 @@
+#include "HitBox.h"
+
+
+
+hitBox::hitBox(int x1, int x2, int y1, int y2)
+{
+	this->x1 = x1;
+	this->x2 = x2;
+	this->y1 = y1;
+	this->y2 = y2;
+
+}
+
+hitBox::hitBox()
+{
+	x1 = 0;
+	x2 = 0;
+	y1 = 0;
+	y2 = 0;
+}
+
+int hitBox::getLargeur()
+{
+	return x2 - x1;
+
+}
+
+int hitBox::getHauteur()
+{
+	return y2 - y1;
+}
+
+bool hitBox::is_in(const int x, const int y)
+{
+	if (x >= x1 && x <= x2 && y >= y1 && y <= y2) return true;
+	return false;
+}

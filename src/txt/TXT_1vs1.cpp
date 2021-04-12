c6347ce257a6530f003a61a3f9733e0865f5dbb9
FREW SAMUEL p1914522 <samuel.frew@etu.univ-lyon1.fr>
2021-04-12 21:26:16 +0200

    Débugage mode 1vs1

--- src/txt/TXT_1vs1.cpp
+++ src/txt/TXT_1vs1.cpp
@@ -33,89 +33,100 @@ void TXT_1vs1::boucle () {
     Ajout des chronos dans un deuxieme temps
     */
 
+    bool stop1 = false;
+    bool stop2 = false;
     bool stop = false;
+    bool valOK = false;
     int valeurEntree, l, c;
+
+    //La grille qu'on utilise pour stocker celle de J1
     Grille grilleJ1 (jeu.grilleSolution.dim);
 
     if (!jeu.initDone) {
         jeu.init();
+        cout << "Grille generee"<<endl;
         jeu.initDone = true;
     }
 
+    //BOUCLE DE JEU PRINCIPALE
     do {
-        termClear();
-
-        jeu.grilleJeu.grille.print();
-
         //Premiere boucle de jeu
+        /* On reste ds cette boucle tant que J1 n'a pas rempli la grille en entier
+        */
         do {
-            //saisie de la valeur a placer
-            
-            cout << "Quelle valeur voulez-vous placer ?"<< endl;
-            cin >> valeurEntree;
 
-        } while (!jeu.estValValide((unsigned char)valeurEntree) && !stop);
+            termClear();
+            jeu.grilleJeu.grille.print();
 
-        if (!stop) {
+            //Saisie de la valeur a placer
+            do {
+                cout << "J1 : Quelle valeur voulez-vous placer ?"<< endl;
+                cin >> valeurEntree;
+            } while (!jeu.estValValide((unsigned char)valeurEntree) && !stop);
+
             //placement de la valeur, etc
             //grille pleine -> enregistrement
-
-            unsigned char lc, cc;
             do {
-               cout<<"Sur quelle ligne ? "<<endl;
-            cin >>l;
-            cout<<"Et quelle colonne ?"<<endl;
-            cin >> c;
-            lc = (unsigned char)l;
-            cc = (unsigned char)c; 
-            } while (!jeu.sontCorValides(lc, cc));
-            
+                cout<<"J1 : Sur quelle ligne ? "<<endl;
+                cin >>l;
+                cout<<"J1 : Et quelle colonne ?"<<endl;
+                cin >> c;
+
+                valOK = jeu.sontCorValides(l, c);
+            } while (!valOK);
+                
             jeu.grilleJeu.setCase(l - 1, c - 1, valeurEntree); //on place la valeur ds la grille
 
             //Si la grille est pleine
             if (jeu.verifGrillePleine(jeu.grilleJeu)) {
+                termClear();
+                jeu.grilleJeu.grille.print();
+                stop1 = true;
                 //On copie la grille dans une grille temporaire
                 grilleJ1 = jeu.grilleJeu;
                 //On remet la grille de jeu comme au départ
                 jeu.grilleJeu = jeu.grilleOriginale;
-                termClear();
-                cout<<"Grille remplie : au tour du joueur suivant !"<<endl;  
+                
             }
-        }
-        
-        //Deuxieme boucle de jeu
+        } while (!stop1);
 
-        //remettre la grille de jeu a 0
+        cout<<"J1 : Grille remplie : au tour du joueur suivant !"<<endl;
+
+        //Deuxième boucle de jeu
         do {
-            //saisie de la valeur a placer
-            
-            cout << "Quelle valeur voulez-vous placer ?"<< endl;
-            cin >> valeurEntree;
 
-        } while (!jeu.estValValide((unsigned char)valeurEntree) && !stop);
+            termClear();
+            jeu.grilleJeu.grille.print();
 
-        if (!stop) {
+            do {
+                //saisie de la valeur a placer
+                cout << "J2 : Quelle valeur voulez-vous placer ?"<< endl;
+                cin >> valeurEntree;
+
+            } while (!jeu.estValValide((unsigned char)valeurEntree) && !stop);
+
             //placement de la valeur, etc
             //grille pleine -> affichage des grilles et solution
 
-            unsigned char lc, cc;
             do {
-               cout<<"Sur quelle ligne ? "<<endl;
-            cin >>l;
-            cout<<"Et quelle colonne ?"<<endl;
-            cin >> c;
-            lc = (unsigned char)l;
-            cc = (unsigned char)c; 
-            } while (!jeu.sontCorValides(lc, cc));
-            
+                cout<<"J2 : Sur quelle ligne ? "<<endl;
+                cin >>l;
+                cout<<"J2 : Et quelle colonne ?"<<endl;
+                cin >> c;
+
+                valOK = jeu.sontCorValides(l, c);
+            } while (!valOK);
+                
             jeu.grilleJeu.setCase(l - 1, c - 1, valeurEntree); //on place la valeur ds la grille
 
             //Si la grille est pleine
             if (jeu.verifGrillePleine(jeu.grilleJeu)) {
                 stop = true;
+                stop2 = true;
                 termClear();
-                cout<<"Grille remplie : partie terminée !"<<endl;
-                
+
+                cout<<"J2 : Grille remplie : partie terminée !"<<endl;
+                    
                 cout<<"Grille du joueur 1 :"<<endl;
                 grilleJ1.grille.print();
 
@@ -125,8 +136,9 @@ void TXT_1vs1::boucle () {
                 cout<<"Grille solution :"<<endl;
                 jeu.grilleSolution.grille.print();
             }
-        }
+        } while (!stop2);  
     } while (!stop);
+    //PB allocation memoire a la fin
 }
 
 unsigned char TXT_1vs1::menu () const {

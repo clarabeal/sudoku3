1f60ff7a82f82df08170b56b96702e3f7057ae82
FREW SAMUEL p1914522 <samuel.frew@etu.univ-lyon1.fr>
2021-04-19 12:59:25 +0200

    Ajout menu 1vs1

--- src/txt/TXT_1vs1.cpp
+++ src/txt/TXT_1vs1.cpp
@@ -6,11 +6,9 @@
 using namespace std;
 
 TXT_1vs1::TXT_1vs1(unsigned char d) : jeu(d) {
-
 }
 
 TXT_1vs1::~TXT_1vs1 () {
-
 }
 
 void TXT_1vs1::termClear () const {
@@ -37,10 +35,14 @@ void TXT_1vs1::boucle () {
     bool stop2 = false;
     bool stop = false;
     bool valOK = false;
-    int valeurEntree, l, c;
+    bool choix;
+    int valeurEntree, l, c, nb1, nb2;
+    unsigned char resMenu;
+
+    int dim = jeu.grilleJeu.dim;
 
     //La grille qu'on utilise pour stocker celle de J1
-    Grille grilleJ1 (jeu.grilleSolution.dim);
+    Grille grilleJ1 (dim);
 
     if (!jeu.initDone) {
         jeu.init();
@@ -60,10 +62,72 @@ void TXT_1vs1::boucle () {
 
             //Saisie de la valeur a placer
             do {
-                cout << "J1 : Quelle valeur voulez-vous placer ?"<< endl;
+                cout << "J1 : Quelle valeur voulez-vous placer ? | Menu : "<<dim + 1<< endl;
                 cin >> valeurEntree;
-            } while (!jeu.estValValide((unsigned char)valeurEntree) && !stop);
 
+                //Verification que la valeur saisie est OK
+                if (jeu.estValValide((unsigned char)valeurEntree) || (valeurEntree == dim + 1)) {
+                    valOK = true;
+                }
+            
+            } while (!valOK && !stop);
+                //Si le joueur fait appel a une des fonctionnalites du menu
+                if (valeurEntree == dim + 1) {
+                resMenu = menu ();
+                switch (resMenu) {
+                    case 0 :
+                        //Recommencer la meme grille
+                        jeu.grilleJeu.grille = jeu.grilleOriginale.grille;
+                        termClear();
+                        jeu.grilleJeu.grille.print();
+                        cout<<endl;
+                        break;
+                    case 1 :
+                        //Generer une nouvelle grille
+                        jeu.init();
+                        termClear();
+                        jeu.grilleJeu.grille.print();
+                        cout<<endl;
+                        break;
+                    case 2 :
+                        //Abandonner et afficher solution
+                        termClear();
+                        cout<<"Abandon : partie terminee !"<<endl<<"Votre Grille :"<<endl;
+                        jeu.grilleJeu.grille.print();
+                        cout<<endl<<"Grille solution :"<<endl;
+                        jeu.grilleSolution.grille.print();
+                        stop1= true;
+                        stop = true;
+                        break;
+                    case 3 :
+                        //Quitter et retour au menu
+                        do {
+                            termClear();
+                            cout << "||||||||||||||||||||||||||||||||| SUDOKU 3 |||||||||||||||||||||||||||||||||" << endl;
+                            cout << "||                                                                        ||" << endl;
+                            cout << "|| Voulez vous vraiment quitter la partie?                                ||" << endl;
+                            cout << "||                                                                        ||" << endl;
+                            cout << "|| 1: OUI                                                                 ||" << endl;
+                            cout << "|| 0: NON                                                                 ||" << endl;
+                            cout << "||                                                                        ||" << endl;
+                            cout << "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+                            cout << "Votre choix: ";
+                            cin >> choix;
+
+                        } while (choix != 0 && choix != 1);
+                        if (choix) { stop = true; stop1 = true; }
+                        else {
+                            termClear();
+                            jeu.grilleJeu.grille.print();
+                            cout<<endl;
+                        }
+                        break;
+                     default :
+                        //Retour au jeu
+                        cout<<"Retour au jeu !"<<endl;
+                        break;
+                }   
+            }
             //placement de la valeur, etc
             //grille pleine -> enregistrement
             do {
@@ -71,9 +135,7 @@ void TXT_1vs1::boucle () {
                 cin >>l;
                 cout<<"J1 : Et quelle colonne ?"<<endl;
                 cin >> c;
-
-                valOK = jeu.sontCorValides(l, c);
-            } while (!valOK);
+            } while (!jeu.sontCorValides(l, c));
                 
             jeu.grilleJeu.setCase(l - 1, c - 1, valeurEntree); //on place la valeur ds la grille
 
@@ -82,6 +144,7 @@ void TXT_1vs1::boucle () {
                 termClear();
                 jeu.grilleJeu.grille.print();
                 stop1 = true;
+                nb1 = jeu.nbErreurs();
                 //On copie la grille dans une grille temporaire
                 grilleJ1 = jeu.grilleJeu;
                 //On remet la grille de jeu comme au départ
@@ -113,9 +176,7 @@ void TXT_1vs1::boucle () {
                 cin >>l;
                 cout<<"J2 : Et quelle colonne ?"<<endl;
                 cin >> c;
-
-                valOK = jeu.sontCorValides(l, c);
-            } while (!valOK);
+            } while (!jeu.sontCorValides(l, c));
                 
             jeu.grilleJeu.setCase(l - 1, c - 1, valeurEntree); //on place la valeur ds la grille
 
@@ -123,22 +184,25 @@ void TXT_1vs1::boucle () {
             if (jeu.verifGrillePleine(jeu.grilleJeu)) {
                 stop = true;
                 stop2 = true;
+
+                nb2 = jeu.nbErreurs();
                 termClear();
 
                 cout<<"J2 : Grille remplie : partie terminée !"<<endl;
                     
-                cout<<"Grille du joueur 1 :"<<endl;
+                cout<<endl<<"Grille du joueur 1 :"<<endl;
                 grilleJ1.grille.print();
+                cout<<endl<<"J1 a fait "<<nb1<<" erreur(s)"<<endl;
 
-                cout<<"Grille du joueur 2"<<endl;
+                cout<<endl<<"Grille du joueur 2 :"<<endl;
                 jeu.grilleJeu.grille.print();
+                cout<<endl<<"J2 a fait "<<nb2<<" erreur(s)"<<endl;
 
-                cout<<"Grille solution :"<<endl;
+                cout<<endl<<"Grille solution :"<<endl;
                 jeu.grilleSolution.grille.print();
             }
         } while (!stop2);  
     } while (!stop);
-    //PB allocation memoire a la fin
 }
 
 unsigned char TXT_1vs1::menu () const {
@@ -147,25 +211,22 @@ unsigned char TXT_1vs1::menu () const {
     do {
         termClear();
         jeu.grilleJeu.grille.print();
-        cout << "Votre temps ";
-        jeu.chrono.afficher();
+        //cout << "Votre temps ";
+        //jeu.chrono.afficher();
         cout << endl;
         cout << "||||||||||||||||||||||||||||| MENU | SUDOKU 3 ||||||||||||||||||||||||||||||" << endl;
         cout << "||                                                                        ||" << endl;
         cout << "|| 0: Recommencer la meme grille                                          ||" << endl;
         cout << "|| 1: Generer une nouvelle grille                                         ||" << endl;
         cout << "|| 2: Abandonner la partie et afficher la solution                        ||" << endl;
-        cout << "|| 3: Sauvegarder                                                         ||" << endl;
-        cout << "|| 4: Quitter la partie et revenir au menu sans sauvegarder               ||" << endl;
-        cout << "|| 5: Retour au jeu                                                       ||" << endl;
+        cout << "|| 3: Quitter la partie et revenir au menu                                ||" << endl;
+        cout << "|| 4: Retour au jeu                                                       ||" << endl;
         cout << "||                                                                        ||" << endl;
         cout << "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
         cin >> value;
-    } while (value < 0 || value > 5);
+    } while (value < 0 || value > 4);
 
     termClear();
     jeu.grilleJeu.grille.print();
     return value;
 }
-
-/* au debut une seule option : recommencer la grille */
